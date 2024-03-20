5cd334ada35f5193bfbfee81c12c7d2e9606757f
huytuan033 <hahuytuan033@gmail.com>
2024-03-21 04:47:08 +0700

    Add Animation & animtor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,28 +10,60 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
 
-    private void Awake() {
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         playerControls.Enable();
     }
 
-    private void Update() {
+    private void Update()
+    {
         PlayerInput();
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         Move();
+        AdjustPlayerFacingDirection();
     }
 
-    private void PlayerInput() {
+    private void PlayerInput()
+    {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move() {
+    private void Move()
+    {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        // Lấy vị trí của con trỏ chuột
+        Vector3 mousePos = Input.mousePosition;
+        // Chuyển đổi vị trí của nhân vật chính từ tọa độ thế giới sang tọa độ màn hình
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x > playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
     }
 }
